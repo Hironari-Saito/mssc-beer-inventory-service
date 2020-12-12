cfab8bf6aea41778be18297aa7a298b05a0c223d
Hironari-Saito <hiroshi31100@gmail.com>
2020-12-12 23:17:10 +0900

    refactoring package

--- src/main/java/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
+++ src/main/java/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -1,7 +1,7 @@
 package guru.sfg.beer.inventory.service.web.mappers;
 
 import guru.sfg.beer.inventory.service.domain.BeerInventory;
-import guru.sfg.beer.inventory.service.web.model.BeerDto;
+import guru.sfg.brewery.model.BeerDto;
 import org.mapstruct.Mapper;
 
 /**
